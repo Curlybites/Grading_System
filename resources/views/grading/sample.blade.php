9be2809b833aab8936b36e0f531172afab6ff62d
XirTech <john.darlucio022@gmail.com>
2022-11-10 22:36:23 +0800

    gradingForm

--- resources/views/grading/sample.blade.php
+++ resources/views/grading/sample.blade.php
@@ -10,55 +10,162 @@
 </head>
 <body>
   
-    <div class="container mt-4">
-        <h3>TVL/ Sports/ Arts and Design Track</h3>
-    </div>
+   <div class="row mt-4">
+       <div class="col-4  d-flex justify-content-center"> 
+       <img src="/images/amlac.png" class="w-25 h-15"></img>
+       </div>
+       <div class="col-4">
+            <div class="container mt-5 text-center">
+                  <h3>TVL/ Sports/ Arts and Design Track</h3>
+              </div>
+       </div>
+       <div class="col-4 d-flex justify-content-center">
+            <img src="/images/Logo-DepEd-1.png" class="w-25 h-15"></img>
+       </div>
+
+   </div>
           
        <div class="container mt-5">
           
-       <div class="table-responsive">
-    <table class="table table-bordered table-striped" id="myTable">
-      <thead>
-        <tr>
-          <th colspan="4"></th>
-          <th colspan="2" class="text-center">Activities</th>
-          <th colspan="2" class="text-center">Quizzes</th>
-          <th colspan="2" class="text-center">Assignment</th>   
-        </tr>
-        <tr class="text-center bg-secondary">
-          <th>StudNo</th>
-          <th>FirstName</th>
-          <th>LastName</th>
-          <th>MiddleName</th>
-          <th class="text-center">Act1</th>
-          <th class="text-center">Act2</th>
-          <th class="text-center">Ass1</th>
-          <th class="text-center">Ass2</th>
-          <th class="text-center">Q1</th>
-          <th class="text-center">Q2</th>
-          <th class="text-center">PROJ</th>
-          <th class="text-center">EXAM</th></th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>1</td>
-          <td>Prince Carlz </td>
-          <td>Darlucio</td>
-          <td>Sample gabril</td>
-          <td><input type="number" class="" id="" name="" style="width: 65px;" min="1" max="1000" placeholder="Act1"></td>
-          <td><input type="number" class="" id="" name="" style="width: 60px;" min="1" max="1000" placeholder="Act2"></td>
-          <td><input type="number" class="" id="" name="" style="width: 60px;" min="1" max="1000" placeholder="Q1"></td>
-          <td><input type="number" class="" id="" name="" style="width: 60px;" min="1" max="1000" placeholder="Q2"></td>
-          <td><input type="number" class="" id="" name="" style="width: 60px;" min="1" max="1000" placeholder="A1"></td>
-          <td><input type="number" class="" id="" name="" style="width: 60px;" min="1" max="1000" placeholder="A2"></td>
-          <td><input type="number" class="" id="" name="" style="width: 60px;" min="1" max="1000" placeholder="Proj"></td>
-          <td><input type="number" class="" id="" name="" style="width: 60px;" min="1" max="1000" placeholder="Exam"></td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
-</div>
+               <div class="table-responsive">
+
+                      <table class="table table-bordered table-striped table-hover">
+                                <thead class="">
+                                <tr>
+                                    <th class="text-center" rowspan="2" colspan="5">FIRST QUARTER</th>
+                                    <th class="" rowspan="2" colspan="13">Grade & Section: </th>
+                                    <th colspan="13">TEACHER:</th>
+                                    <th colspan="5">SUBJECT:</th>
+                                    <tr>
+                                        <th colspan="13">SEMESTER:</th>
+                                        <th colspan="5">TRACK:</th>
+                                    </tr>
+                                </tr>
+                                <tr>
+                                        <th rowspan="2" colspan="5">LEARNER'S NAMES</th>
+                                        <th colspan="13" class="text-center">WRITTEN WORK (25%)</th>
+                                        <th colspan="13" class="text-center">PERFORMANCE TASK (50%)</th>
+                                        <th colspan="3">QUARTERLY ASSESMENT (25%)</th>
+                                        <th rowspan="3">INITIAL GRADE</th>
+                                        <th rowspan="3">QUARTERLY GRADE</th>
+                                </tr>
+                                <tr class="text-center">
+                                        <!--Written Task  -->
+                                        <th>1</th>
+                                        <th>2</th>
+                                        <th>3</th>
+                                        <th>4</th>
+                                        <th>5</th>
+                                        <th>6</th>
+                                        <th>7</th>
+                                        <th>8</th>
+                                        <th>9</th>
+                                        <th>10</th>
+                                        <th>TOTAL</th>
+                                        <th>PS</td>
+                                        <th>WS</th>
+                                        <!-- performance task -->
+                                        <th>1</th>
+                                        <th>2</th>
+                                        <th>3</th>
+                                        <th>4</th>
+                                        <th>5</th>
+                                        <th>6</th>
+                                        <th>7</th>
+                                        <th>8</th>
+                                        <th>9</th>
+                                        <th>10</th>
+                                        <th>TOTAL</th>
+                                        <th>PS</td>
+                                        <th>WS</th>
+                                        <!-- Quarterly Assesment  -->
+                                        <th>1</th>
+                                        <th>2</th>
+                                        <th>3</th>
+                                </tr>
+                                <tr class="text-center">
+                                        <th colspan="5">HPG</th>
+                                        <!--Written Task  -->
+                                        <th></th>
+                                        <th></th>
+                                        <th></th>
+                                        <th></th>
+                                        <th></th>
+                                        <th></th>
+                                        <th></th>
+                                        <th></th>
+                                        <th></th>
+                                        <th></th>
+                                        <th></th>
+                                        <th>100.00</td>
+                                        <th>25%</th>
+                                        <!-- performance task -->
+                                        <th></th>
+                                        <th></th>
+                                        <th></th>
+                                        <th></th>
+                                        <th></th>
+                                        <th></th>
+                                        <th></th>
+                                        <th></th>
+                                        <th></th>
+                                        <th></th>
+                                        <th></th>
+                                        <th>100.00</td>
+                                        <th>50%</th>
+                                        <!-- Quarterly Assesment  -->
+                                        <th>1</th>
+                                        <th>100.00</th>
+                                        <th>25%</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <tr>
+                                    <!-- Written Task -->
+                                    <td colspan="5">Darlucio, John</td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <!-- Performance Task -->
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <!-- Quarterly -->
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <!-- Initial Grade -->
+                                    <td></td>
+                                    <!-- Quarterly Grade -->
+                                    <td></td>
+                                </tr>
+
+                                
+                                
+                                </tbody>
+                            </table>
+              </div>
+    </div>
 
                 </div>
             </div>
