f83a612a53df2141dad7e51bb42dd22ee58a8070
XirTech <john.darlucio022@gmail.com>
2022-11-17 18:39:33 +0800

    remove testdata

--- routes/web.php
+++ routes/web.php
@@ -75,7 +75,6 @@ Route::controller(ClassController::class)->group(function(){
     Route::get('/Class/','classResult');
     Route::get('/filterdata','filterdata');
     Route::get('/store-class','showClass');
-    Route::get('/Class','testdata');
 });
 
 Route::controller(ProfessorController::class)->group(function(){
